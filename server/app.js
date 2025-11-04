eb69035c1b9c8217db47b9755b7c16cc3c4b50f2
S11UB11AYAN <shubhayanbagchi30@gmail.com>
2025-11-04 20:06:05 +0530

    feat(auth): implment captain registration API

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const connectToDb = require("./db/db");
 const userRoutes = require("./routes/user.route");
+const captainRoutes = require("./routes/captain.route");
 connectToDb();
 
 app.use(cors());
@@ -19,5 +20,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/users", userRoutes);
+app.use("/captains", captainRoutes);
 
 module.exports = app;
