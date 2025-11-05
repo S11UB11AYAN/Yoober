25ec16d2d452454b25785ea7f8b0ffa6357c12fe
S11UB11AYAN <shubhayanbagchi30@gmail.com>
2025-11-05 16:54:48 +0530

    feat(frontend): initialize Vite app with auth pages and context setup

---
+++ client/src/pages/CaptainLogin.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const CaptainLogin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [captainData, setCaptainData] = useState({});
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setCaptainData({
+      email: email,
+      password: password,
+    });
+    setEmail("");
+    setPassword("");
+  };
+  return (
+    <div className="p-7 flex flex-col justify-between h-screen">
+      <div>
+        <img
+          className="w-16 mb-10"
+          src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
+          alt=""
+        />
+        <form
+          onSubmit={(e) => {
+            submitHandler(e);
+          }}
+        >
+          <h3 className="text-lg font-medium mb-2">What's your email?</h3>
+          <input
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            type="email"
+            required
+            className="bg-[#eee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
+            placeholder="email@example.com"
+          />
+          <h3 className="text-lg font-medium mb-2">Enter Password</h3>
+          <input
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            className="bg-[#eee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
+            type="password"
+            placeholder="password"
+            required
+          />
+          <button className="bg-[#111] text-white font-semibold mb-2 rounded px-4 py-2 border w-full text-lg placeholder:text-base">
+            Login
+          </button>
+        </form>
+        <p className="text-center">
+          Join a fleet?{" "}
+          <Link to="/captain-signup" className="text-blue-600">
+            Register as a Captain
+          </Link>
+        </p>
+      </div>
+      <div>
+        <Link
+          to="/login"
+          className="bg-[#d5622d] flex items-center justify-center text-white mb-5 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
+        >
+          Sign in a User
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default CaptainLogin;
---
+++ client/src/pages/CaptainSignup.jsx
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const CaptainSignup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [userData, setUserData] = useState({});
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setUserData({
+      fullName: {
+        firstName: firstName,
+        lastName: lastName,
+      },
+      email: email,
+      password: password,
+    });
+    setEmail("");
+    setFirstName("");
+    setLastName("");
+    setPassword("");
+  };
+  return (
+    <div className="p-7 flex flex-col justify-between h-screen">
+      <div>
+        <img
+          className="w-16 mb-10"
+          src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
+          alt=""
+        />
+        <form
+          onSubmit={(e) => {
+            submitHandler(e);
+          }}
+        >
+          <h3 className="text-lg font-medium mb-2">
+            What's our Captain's name?
+          </h3>
+          <div className="flex gap-4 mb-6">
+            <input
+              type="text"
+              required
+              className="bg-[#eee] w-1/2  rounded px-4 py-2 border  text-lg placeholder:text-base"
+              placeholder="Firstname"
+              value={firstName}
+              onChange={(e) => {
+                setFirstName(e.target.value);
+              }}
+            />
+            <input
+              type="text"
+              required
+              className="bg-[#eee] w-1/2 rounded px-4 py-2 border  text-lg placeholder:text-base"
+              placeholder="Lastname"
+              value={lastName}
+              onChange={(e) => {
+                setLastName(e.target.value);
+              }}
+            />
+          </div>
+          <h3 className="text-lg font-medium mb-2">
+            What's our Captain's email?
+          </h3>
+          <input
+            type="email"
+            required
+            className="bg-[#eee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
+            placeholder="email@example.com"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <h3 className="text-lg font-medium mb-2">Enter Password</h3>
+          <input
+            className="bg-[#eee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
+            type="password"
+            placeholder="password"
+            required
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <button className="bg-[#111] text-white font-semibold mb-2 rounded px-4 py-2 border w-full text-lg placeholder:text-base">
+            Sign up
+          </button>
+        </form>
+        <p className="text-center">
+          Already have a account?{" "}
+          <Link to="/captain-login" className="text-blue-600">
+            Login here
+          </Link>
+        </p>
+      </div>
+      <div>
+        <p className="text-[10px]">
+          By proceeding, you consent to get mails, by automated means, from
+          Yoober and its affiliates to the email provided
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default CaptainSignup;
